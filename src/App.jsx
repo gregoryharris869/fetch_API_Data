808201d11b3309a1d625001f8320a0ef554145a8
Gregory Harris <63122765+gregoryharris869@users.noreply.github.com>
2023-12-14 23:49:52 -0600

    Started api data

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState, useEffect } from "react";
+
+function App() {
+  const API_URL = "https://jsonplaceholder.typicode.com/";
+  return (
+    <>
+      <div className="App"></div>
+    </>
+  );
+}
+
+export default App;
